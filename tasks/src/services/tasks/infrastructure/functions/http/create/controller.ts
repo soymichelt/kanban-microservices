38667dc05b020d8c829533f31368331ddb52b607
SoymichelDev <mtraatabladaa94@gmail.com>
2024-01-18 19:14:58 -0600

    refactor: update response to CreateTaskController

--- tasks/src/services/tasks/infrastructure/functions/http/create/controller.ts
+++ tasks/src/services/tasks/infrastructure/functions/http/create/controller.ts
@@ -1,7 +1,6 @@
 import { TaskResponse } from '@services/tasks/application/responses/taskResponse';
 import { CreateTaskRequest } from '@services/tasks/application/useCases/create/createTaskRequest';
 import { CreateTaskUseCase } from '@services/tasks/application/useCases/create/createTaskUseCase';
-import { TaskPrimitives } from '@services/tasks/domain/task';
 import { BaseController } from '@shared/infrastructure/controllers/baseController';
 import { inject, injectable } from 'tsyringe';
 
@@ -11,7 +10,7 @@ export class CreateTaskController extends BaseController<CreateTaskRequest, Task
     super();
   }
 
-  public async run(request: CreateTaskRequest): Promise<TaskPrimitives> {
+  public async run(request: CreateTaskRequest): Promise<TaskResponse> {
     const result = await this.useCase.run(request);
     return result;
   }
